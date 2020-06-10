e10901461ac62ce2d209d128c2d0e5174e8f4bbf
James Devine <j.devine@lancaster.ac.uk>
2020-06-10 15:46:35 +0100

    add simple rx interface

---
+++ implementation/simple_rx.c
@@ -0,0 +1,22 @@
+static jd_frame_t *frameToHandle;
+
+void jd_rx_init(void) {
+}
+
+int jd_rx_frame_received(jd_frame_t *frame) {
+    if (frameToHandle)
+        return -1;
+    frameToHandle = frame;
+    return 0;
+}
+
+jd_frame_t* jd_rx_get_frame(void) {
+    jd_frame_t* f = NULL;
+
+    if (frameToHandle) {
+        f = frameToHandle;
+        frameToHandle = NULL;
+    }
+
+    return f;
+}
