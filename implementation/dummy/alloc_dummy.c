545a8da81754ba616b1e57b4d9f58c7e76dea20a
James Devine <j.devine@lancaster.ac.uk>
2020-06-10 15:08:58 +0100

    Add api descriptions and a dummy implementation for allocator.

---
+++ implementation/dummy/alloc_dummy.c
@@ -0,0 +1,20 @@
+#include "interfaces/jd_alloc.h"
+#include "jd_util.h"
+#include "interfaces/jd_hw.h"
+
+__attribute__((weak)) void jd_alloc_stack_check(void) {
+}
+
+__attribute__((weak)) void jd_alloc_init(void) {
+}
+
+__attribute__((weak)) void *jd_alloc(uint32_t size) {
+    return NULL;
+}
+
+__attribute__((weak)) void jd_free(void* ptr) {
+}
+
+__attribute__((weak)) void *jd_alloc_emergency_area(uint32_t size) {
+    return NULL;
+}
--- inc/interfaces/jd_alloc.h
+++ inc/interfaces/jd_alloc.h
@@ -2,10 +2,25 @@
 #define __JD_ALLOC_H
 
 #include "jd_config.h"
+// Allocator implementations are only required if an app or transmission/reception requires dynamic allocation
+// Implementors can map these APIs onto custom malloc/free implementations
+// A dummy implementation can be optionally compiled in, and specific function overriden (implementation/dummy/alloc.c)
 
-// only required if app, or queuing mechanisms require dynamic allocation
+/**
+ * This function is invoked by a call to jd_init.
+ **/
 void jd_alloc_init(void);
-void *jd_alloc(uint32_t size);
+
+/**
+ * Any dynamic allocations made by the jacdac-c library will use this function.
+ **/
+void* jd_alloc(uint32_t size);
+
+/**
+ *  Any corresponding free calls made by the jacdac-c library will use this function.
+ **/
+void jd_free(void* ptr);
+
 void jd_alloc_stack_check(void);
 void *jd_alloc_emergency_area(uint32_t size);
 
