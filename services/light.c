315e7eb95d44f6685ffabfe7fb4e37a36f79c042
Michal Moskal <michal@moskal.me>
2021-01-06 15:59:05 +0100

    Improvement to brightness limits

--- services/light.c
+++ services/light.c
@@ -249,22 +249,25 @@ static void limit_intensity(srv_t *state) {
     uint8_t *d = (uint8_t *)state->pxbuffer;
     unsigned n = state->numpixels * 3;
     int prev_intensity = state->intensity;
+    int intensity = state->intensity;
 
-    if (state->requested_intensity > state->intensity)
-        state->intensity += 1 + (state->intensity >> 5);
-    if (state->intensity > state->requested_intensity)
-        state->intensity = state->requested_intensity;
+    intensity += 1 + (intensity >> 5);
+    if (intensity > state->requested_intensity)
+        intensity = state->requested_intensity;
 
     int current_full = 0;
     int current = 0;
+    int current_prev = 0;
     while (n--) {
         uint8_t v = *d++;
-        current += SCALE0(v, state->intensity);
+        current += SCALE0(v, intensity);
+        current_prev += SCALE0(v, prev_intensity);
         current_full += v;
     }
 
     // 46uA per step of LED
     current *= 46;
+    current_prev *= 46;
     current_full *= 46;
 
     // 14mA is the chip at 48MHz, 930uA per LED is static
@@ -272,17 +275,20 @@ static void limit_intensity(srv_t *state) {
     int current_limit = state->maxpower * 1000 - base_current;
 
     if (current <= current_limit) {
+        state->intensity = intensity;
         // LOG("curr: %dmA; not limiting %d", (base_current + current) / 1000, state->intensity);
         return;
+    }
+
+    if (current_prev <= current_limit) {
+        return; // no change needed
     }
 
     int inten = current_limit / (current_full >> 8) - 1;
     if (inten < 0)
         inten = 0;
-    // only display message if the limit wasn't just a result of relaxing above
-    if (inten < prev_intensity)
-        LOG("limiting %d -> %d; %dmA", state->intensity, inten,
-            (base_current + (current_full * inten >> 8)) / 1000);
+    LOG("limiting %d -> %d; %dmA", state->intensity, inten,
+        (base_current + (current_full * inten >> 8)) / 1000);
     state->intensity = inten;
 }
 
