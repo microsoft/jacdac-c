2c2ecc453ecbeaceeb9666e23eef529c457ef07e
Tom Ball <tballmsft@users.noreply.github.com>
2021-03-29 10:06:12 -0700

    update button implementation

--- services/button.c
+++ services/button.c
@@ -7,23 +7,15 @@
 
 struct srv_state {
     SENSOR_COMMON;
-    uint32_t click_hold_time;
     uint8_t pressed;
     uint8_t pin;
     uint8_t backlight_pin;
     uint8_t prev_pressed;
     uint8_t active;
-    uint32_t prev_presslen;
+    uint32_t next_hold;
     uint32_t press_time;
     uint32_t nextSample;
 };
-
-REG_DEFINITION(                             //
-    button_regs,                            //
-    REG_SENSOR_COMMON,                      //
-    REG_U32(JD_BUTTON_REG_CLICK_HOLD_TIME), //
-    REG_U8(JD_BUTTON_REG_PRESSED),          //
-)
 
 static void update(srv_t *state) {
     state->pressed = pin_get(state->pin) == state->active;
@@ -34,22 +26,18 @@ static void update(srv_t *state) {
         if (state->pressed) {
             jd_send_event(state, JD_BUTTON_EV_DOWN);
             state->press_time = now;
-            state->prev_presslen = 0;
+            state->next_hold = 500000;
         } else {
             jd_send_event(state, JD_BUTTON_EV_UP);
-            if (state->prev_presslen < state->click_hold_time * 1000) {
-                jd_send_event_ext(state, JD_BUTTON_EV_CLICK, &(state->prev_presslen),
-                                  sizeof(state->prev_presslen));
-            }
         }
     }
 
     if (state->pressed) {
         uint32_t presslen = now - state->press_time;
-        if (presslen >= state->click_hold_time * 1000 &&
-            state->prev_presslen < state->click_hold_time * 1000)
+        if (presslen >= state->next_hold) {
             jd_send_event(state, JD_BUTTON_EV_HOLD);
-        state->prev_presslen = presslen;
+            state->next_hold = presslen + 500000;
+        }
     }
 }
 
@@ -61,22 +49,13 @@ void btn_process(srv_t *state) {
 }
 
 void btn_handle_packet(srv_t *state, jd_packet_t *pkt) {
-    if (sensor_handle_packet_simple(state, pkt, &state->pressed, sizeof(state->pressed)))
-        return;
-
-    switch (service_handle_register(state, pkt, button_regs)) {
-    case JD_BUTTON_REG_CLICK_HOLD_TIME:
-        if (state->click_hold_time < 500)
-            state->click_hold_time = 500;
-        break;
-    }
+    sensor_handle_packet_simple(state, pkt, &state->pressed, sizeof(state->pressed));
 }
 
 SRV_DEF(btn, JD_SERVICE_CLASS_BUTTON);
 
 void btn_init(uint8_t pin, bool active, uint8_t backlight_pin) {
     SRV_ALLOC(btn);
-    state->click_hold_time = 500;
     state->pin = pin;
     state->backlight_pin = backlight_pin;
     state->active = active;
