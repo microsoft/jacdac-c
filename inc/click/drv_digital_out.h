6e788cf7b50df8fb9940449432138cc89549c460
Michal Moskal <michal@moskal.me>
2021-08-05 13:37:37 -0700

    First draft of MikroE click compat layer

---
+++ inc/click/drv_digital_out.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "drv_name.h"
+
+typedef struct {
+    uint8_t pin;
+} digital_out_t;
+
+static inline int digital_out_init(digital_out_t *out, pin_name_t name) {
+    out->pin = name;
+    pin_setup_output(name);
+    return 0;
+}
+
+static inline void digital_out_high(digital_out_t *out) {
+    pin_set(out->pin, 1);
+}
+
+static inline void digital_out_low(digital_out_t *out) {
+    pin_set(out->pin, 0);
+}
+
+static inline void digital_out_toggle(digital_out_t *out) {
+    pin_toggle(out->pin);
+}
+
+static inline void digital_out_write(digital_out_t *out, uint8_t value) {
+    pin_set(out->pin, value);
+}
---
+++ inc/click/drv_name.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "jd_drivers.h"
+
+typedef int pin_name_t;
+typedef int err_t;
+
+#define HAL_PIN_NC (-1)
