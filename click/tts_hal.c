f96072f8cb2e658b6f042c322dbbf64939ea334b
James Devine <t-jamdev@microsoft.com>
2021-09-09 17:58:20 +0100

    use mikrobus pin defs

--- click/tts_hal.c
+++ click/tts_hal.c
@@ -19,7 +19,6 @@
 * Includes
 *******************************************************************************/
 #include "tts_hal.h"
-#include "board.h"
 #include "drv_name.h"
 #include "tinyhw.h"
 #include "lib.h"
@@ -49,49 +48,35 @@
 *******************************************************************************/
 void tts_hal_cs_high()
 {
-#if MIKROBUS_AVAILABLE
-    pin_set(PIN_RX_CS,1);
-#endif
+    pin_set(MIKROBUS_CS,1);
 }
 
 void tts_hal_cs_low()
 {
-#if MIKROBUS_AVAILABLE
-    pin_set(PIN_RX_CS,0);
-#endif
+    pin_set(MIKROBUS_CS,0);
 }
 
 void tts_hal_mut_high()
 {
-#if MIKROBUS_AVAILABLE
-    pin_set(PIN_AN,1);
-#endif
+    pin_set(MIKROBUS_AN,1);
 }
 
 void tts_hal_mut_low()
 {
-#if MIKROBUS_AVAILABLE
-    pin_set(PIN_AN,0);
-#endif
+    pin_set(MIKROBUS_AN,0);
 }
 
 void tts_hal_reset( void )
 {
-#if MIKROBUS_AVAILABLE
-    pin_set(PIN_RST,0);
+    pin_set(MIKROBUS_RST,0);
     Delay_10ms();
-    pin_set(PIN_RST,1);
+    pin_set(MIKROBUS_RST,1);
     Delay_ms( POR_TIME );
-#endif
 }
 
 bool tts_hal_msg_rdy( void )
 {
-#if MIKROBUS_AVAILABLE
-    return pin_get(PIN_INT);
-#else
-    return 0;
-#endif
+    return pin_get(MIKROBUS_INT);
 }
 
 void tts_hal_init()
