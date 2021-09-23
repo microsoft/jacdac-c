2bc910ed2117ec23fe89e89f798a1909ecabef0b
James Devine <t-jamdev@microsoft.com>
2021-09-23 11:50:22 +0100

    update driver code

--- drivers/ncv7726b.c
+++ drivers/ncv7726b.c
@@ -23,39 +23,56 @@ static uint32_t driver_state_en = 0;
 static uint32_t driver_state_ls = 0;
 static uint32_t driver_state_hs = 0;
 
+static uint32_t ncv7726b_tx = 0;
 static uint32_t ncv7726b_resp = 0;
+static uint16_t command = 0;
+static uint16_t previous_command = 0;
 static volatile uint8_t tx_complete = 0;
 
 static void ncv7726b_reset(void);
+
+// static inline uint8_t flip_word(uint8_t v) {
+//     return ((v & 0xf0) >> 4) | ((v & 0xf) << 4);
+// }
+
+// static inline uint16_t flip (uint16_t v) {
+
+//     uint8_t upper =((v & 0xff00) >> 8);
+//     uint8_t lower = v & 0xff; 
+
+//     return flip_word(upper) | (flip_word(lower) << 8);
+// }
 
 static inline uint16_t flip (uint16_t v) {
     return ((v & 0xff00) >> 8) | ((v & 0x00ff) << 8);
 }
 
 static void ncv7726b_xfer_done(void) {
-    pin_set(PIN_RX_CS,1);
-    tx_complete = 1;
     ncv7726b_resp = flip(ncv7726b_resp);
 
-    DMESG("R: %x", ncv7726b_resp);
+    // DBG("P: %x R: %x", previous_command, ncv7726b_resp);
     // thermal warning
-    if (ncv7726b_resp & 0x0001)
-        jd_panic();
+    // if (ncv7726b_resp & 0x0001)
+    //     jd_panic();
+
+    previous_command = command;
+    pin_set(PIN_RX_CS,1);
+    tx_complete = 1;
 }
 
 static void ncv7726b_send(uint16_t* data) {
     tx_complete = 0;
-    *data = flip(*data); // ls bit first
-    DBG("WRITE %x", *data); 
+    command = *data;
+    ncv7726b_tx = flip(*data); // ls bit first
     pin_set(PIN_RX_CS, 0);
     target_wait_us(10);
-    dspi_xfer(data, &ncv7726b_resp, 2, ncv7726b_xfer_done);
+    dspi_xfer(&ncv7726b_tx, &ncv7726b_resp, 2, ncv7726b_xfer_done);
     while(tx_complete == 0);
 }
 
 static void ncv7726b_write_state(void) {
     // calculate the lower 16 bits
-    uint16_t lower = OVLO;
+    uint16_t lower = 0;// OVLO;
     DBG("EN %x Hs %x LS %x", driver_state_en, driver_state_hs, driver_state_ls);
     // OUT channels aren't indexed from one...
     for (int i = 1; i < 7; i++) {
@@ -83,13 +100,13 @@ static void ncv7726b_write_state(void) {
     }
 
     // calculate the upper 16 bits
-    uint16_t upper = HB_SEL | OVLO;
+    uint16_t upper = HB_SEL;// | OVLO;
 
     for (int i = 7; i < OUT_COUNT + 1; i++) {
         // DBG("COL%d EN %d", i, driver_state_en & (1 << i));
         if (driver_state_en & (1 << i)) {
             upper |= EN_START << (i - 7);
-            DBG("ENH %x", EN_START << (i - 7));
+            DBG("ENH %x", 1 << ((i - 7) + EN_START));
             // drive direction needs to be explicit otherwise driver could 
             // cause attached component to get hot.
             if (!(driver_state_ls & (1 << i)) && !(driver_state_hs & (1 << i)))
@@ -97,14 +114,14 @@ static void ncv7726b_write_state(void) {
 
             if (driver_state_ls & (1 << i))
             {
-                DBG("LSH %x", EN_START << (i - 7));
+                DBG("LSH %x", CNF_START << (i - 7));
                 upper &= ~(CNF_START << (i - 7)); // subtract 7 so the shift is correct (i.e. OUT7 is BIT1 of the upper 16)
             }
 
-            if (driver_state_hs & (1 << i)) 
+            if (driver_state_hs & (1 << i))
             {
                 DBG("HSH %x", CNF_START << (i - 7));
-                upper |= CNF_START << (i - 7); // subtract 7 so the shift is correct
+                upper |= CNF_START << (i - 7); // subtract 1 so the shift is correct
             }
         }
     }
@@ -116,15 +133,15 @@ static void ncv7726b_write_state(void) {
     //     reset =  SRR | HB_SEL;
     //     ncv7726b_send(&reset);
     // } else {
-        DMESG("OUT UP %x", upper);
-        ncv7726b_send(&upper);
-        target_wait_us(3000);
-        DMESG("OUT LOW %x", lower);
+        // DMESG("OUT UP %x", upper);
         ncv7726b_send(&lower);
         target_wait_us(3000);
-    // }
-
-    
+        
+        ncv7726b_send(&upper);
+        target_wait_us(3000);
+        // DMESG("OUT LOW %x", lower);
+        
+        ncv7726b_reset();
 }
 
 static void ncv7726b_channel_set(uint8_t channel, int state) {
@@ -161,20 +178,17 @@ static void ncv7726b_clear_all(void) {
 }
 
 static void ncv7726b_reset(void) {
-    uint16_t reset = ncv7726b_resp | SRR;
+    // uint16_t reset = SRR | OVLO;
+    // ncv7726b_send(&reset);
+    // target_wait_us(3000);
+    uint16_t reset = SRR | HB_SEL | OVLO;
     ncv7726b_send(&reset);
-    target_wait_us(50000);
-    reset = SRR | HB_SEL;
-    ncv7726b_send(&reset);
-    target_wait_us(50000);
+    target_wait_us(3000);
 }
 
 static void ncv7726b_init(void) {
     dspi_init();
     pin_setup_output(PIN_RX_CS);
-
-    uint16_t dummy = 0x8000;
-    ncv7726b_send(&dummy);
     target_wait_us(50000);
     ncv7726b_reset();
     // clean slate
