0c1d989d77547f18dca9fdbb4de2ea07abaa43fc
James Devine <t-jamdev@microsoft.com>
2021-10-08 12:31:46 +0100

    refactor braille to share common driver
    Add character display implementation

--- services/braille_common.c
+++ services/braille_common.c
@@ -1,6 +1,4 @@
-#include "jd_services.h"
-#include "interfaces/jd_pins.h"
-#include "jacdac/dist/c/dotmatrix.h"
+#include "braille_common.h"
 
 #define MAXBRROW 4
 #define MAXBRCOL 8
@@ -17,35 +15,6 @@
 #define BRWAITDIS2 1   // msec for discharge state 2
 #define BRWAITHIZ 50   // msec for high-Z
 #define BRWAITFHIZ 30  // msec for forced high-Z
-
-#define STATE_DIRTY 0x01
-
-#define DOTS_MAX    64
-
-struct srv_state {
-    SRV_COMMON;
-    const hbridge_api_t *api;
-    uint16_t rows;
-    uint16_t cols;
-    uint8_t variant;
-    uint8_t flags;
-    uint16_t padding;
-    uint32_t now;
-    uint8_t dots[DOTS_MAX];
-};
-
-REG_DEFINITION(                                   //
-    braille_regs,                                   //
-    REG_SRV_COMMON,                         //
-    REG_U32(JD_REG_PADDING),                         //
-    REG_U16(JD_DOT_MATRIX_REG_ROWS),                         //
-    REG_U16(JD_DOT_MATRIX_REG_COLUMNS),                         //
-    REG_U8(JD_DOT_MATRIX_REG_VARIANT),                //
-    REG_U8(JD_REG_PADDING),                // flags
-    REG_U16(JD_REG_PADDING),                         //
-    REG_U32(JD_REG_PADDING),                         //
-    REG_BYTES(JD_DOT_MATRIX_REG_DOTS, DOTS_MAX),                         //
-)
 
 const uint16_t BrDotTbl[ MAXBRPHASE ][ MAXBRROW ][ MAXBRCOL ][ MAXBRBANK ] =
  {
@@ -90,17 +59,6 @@ static void delay(uint32_t t) {
     jd_services_sleep_us(t * 1000);
 }
 
-uint16_t tx = 0;
-uint16_t rx = 0;
-int done = 0;
-
-static inline uint16_t flip (uint16_t v) {
-    return ((v & 0xff00) >> 8) | ((v & 0x00ff) << 8);
-}
-
-void BrClrAllDots(srv_t* state);
-void BrSetSingleDot(srv_t* state, uint16_t row, uint16_t col, bool set);
-
 // Braille
 bool BrDotPtn[MAXBRROW][MAXBRCOL] = {false};
 
@@ -119,6 +77,13 @@ void BrClrPtn(void) {
     }
 }
 
+bool BrGetPtn(uint16_t row, uint16_t col) {
+    if (row >= MAXBRROW || col >= MAXBRCOL)
+        hw_panic();
+
+    return BrDotPtn[row][col];
+}
+
 // BrSetPtn: SetBraille Pattern
 void BrSetPtn(uint16_t row, uint16_t col, bool state) {
     if (row >= MAXBRROW || col >= MAXBRCOL)
@@ -128,72 +93,72 @@ void BrSetPtn(uint16_t row, uint16_t col, bool state) {
 }
 
 // BrRfshPtn: Refresh and load new braille pattern
-void BrRfshPtn(srv_t* state) {
+void BrRfshPtn(const hbridge_api_t * api) {
     unsigned char row, col;
 
     // force clear
-    BrClrAllDots(state);
+    BrClrAllDots(api);
 
     // load new pattern
     for (row = 0; row < MAXBRROW; row++) {
         for (col = 0; col < MAXBRCOL; col++) {
             // only set direction
             if (BrDotPtn[row][col] == true) {
-                BrSetSingleDot(state, row, col, true);
+                BrSetSingleDot(api, row, col, true);
             }
         }
     }
 }
 
 // BrClrAllDots: Clear All Dots (forced)
-void BrClrAllDots(srv_t * state) {
+void BrClrAllDots(const hbridge_api_t * api) {
     unsigned char row, col, bk;
 
     for (row = 0; row < MAXBRROW; row++) {
         for (col = 0; col < MAXBRCOL; col++) {
             // reset dot
             for (bk = 0; bk < MAXBRBANK; bk++) {
-                state->api->write_raw(BrDotTbl_forced[BRPHASEDN][row][col][bk]);
+                api->write_raw(BrDotTbl_forced[BRPHASEDN][row][col][bk]);
             }
             delay(BRWAITSETFD);
             // disconnect all cells for pre discharge
-            state->api->write_raw(0x4001);
-            state->api->write_raw(0x0001);
+            api->write_raw(0x4001);
+            api->write_raw(0x0001);
             delay(BRWAITDIS1);
             // discharge ( all connect to LS )
-            state->api->write_raw(0x5F81);
-            state->api->write_raw(0x1F81);
+            api->write_raw(0x5F81);
+            api->write_raw(0x1F81);
             delay(BRWAITDIS2);
             // disconnect all cells
-            state->api->write_raw(0x4001);
-            state->api->write_raw(0x0001);
+            api->write_raw(0x4001);
+            api->write_raw(0x0001);
             delay(BRWAITFHIZ);
         }
     }
 }
 
 // BrSetAllDots: Set All Dots (forced)
-void BrSetAllDots(srv_t * state) {
+void BrSetAllDots(const hbridge_api_t * api) {
     uint16_t row, col, bk;
 
     for (row = 0; row < MAXBRROW; row++) {
         for (col = 0; col < MAXBRCOL; col++) {
             // reset dot
             for (bk = 0; bk < MAXBRBANK; bk++) {
-                state->api->write_raw(BrDotTbl_forced[BRPHASEUP][row][col][bk]);
+                api->write_raw(BrDotTbl_forced[BRPHASEUP][row][col][bk]);
             }
             delay(BRWAITSETFU);
             // disconnect all cells for pre discharge
-            state->api->write_raw(0x4001);
-            state->api->write_raw(0x0001);
+            api->write_raw(0x4001);
+            api->write_raw(0x0001);
             delay(BRWAITDIS1);
             // discharge ( all connect to LS )
-            state->api->write_raw(0x5F81);
-            state->api->write_raw(0x1F81);
+            api->write_raw(0x5F81);
+            api->write_raw(0x1F81);
             delay(BRWAITDIS2);
             // disconnect all cells
-            state->api->write_raw(0x4001);
-            state->api->write_raw(0x0001);
+            api->write_raw(0x4001);
+            api->write_raw(0x0001);
             delay(BRWAITFHIZ);
         }
     }
@@ -201,7 +166,7 @@ void BrSetAllDots(srv_t * state) {
 
 // BrSetSingleDot: set single dot (true: set, false: clear): do NOT use directly, may have
 // instability
-void BrSetSingleDot(srv_t * state, uint16_t row, uint16_t col, bool set) {
+void BrSetSingleDot(const hbridge_api_t * api, uint16_t row, uint16_t col, bool set) {
     uint16_t bk;
     uint8_t ph;
     int delaytime;
@@ -215,7 +180,7 @@ void BrSetSingleDot(srv_t * state, uint16_t row, uint16_t col, bool set) {
 
     // set/clr dot
     for (bk = 0; bk < MAXBRBANK; bk++) {
-        state->api->write_raw(BrDotTbl[ph][row][col][bk]);
+        api->write_raw(BrDotTbl[ph][row][col][bk]);
     }
 
     // load delaytime
@@ -236,100 +201,17 @@ void BrSetSingleDot(srv_t * state, uint16_t row, uint16_t col, bool set) {
     delay(delaytime);
 
     // disconnect all cells for pre discharge
-    state->api->write_raw(0x4001);
-    state->api->write_raw(0x0001);
+    api->write_raw(0x4001);
+    api->write_raw(0x0001);
     delay(BRWAITDIS1);
 
     // discharge ( all connect to LS )
-    state->api->write_raw(0x5F81);
-    state->api->write_raw(0x1F81);
+    api->write_raw(0x5F81);
+    api->write_raw(0x1F81);
     delay(BRWAITDIS2);
 
     // disconnect all cells
-    state->api->write_raw(0x4001);
-    state->api->write_raw(0x0001);
+    api->write_raw(0x4001);
+    api->write_raw(0x0001);
     delay(BRWAITHIZ);
-}
-
-void braille_process(srv_t * state) {
-    if (state->flags & STATE_DIRTY) {
-        BrRfshPtn(state);
-        state->flags &= ~STATE_DIRTY;
-    }
-}
-
-bool get_bit(srv_t * state, uint8_t* data, uint16_t row, uint16_t col) {
-    uint16_t cols_padded = state->cols + (8 - (state->cols % 8));
-    uint32_t bitindex = row * cols_padded + col;
-    uint8_t byte = data[bitindex >> 3];
-    uint8_t bit = bitindex % 8;
-    return (1 == ((byte >> bit) & 1));
-}
-
-// translates from sim to real life accurately.
-#ifdef BRAILLE_V1
-const uint8_t cell_map[] = {2,0,1,3};
-#else
-const uint8_t cell_map[] = {0,1,2,3};
-#endif
-void handle_disp_write(srv_t * state, jd_packet_t* pkt) {
-
-    // update in progress
-    if (state->flags & STATE_DIRTY)
-        return;
-
-    // too big for our buffer
-    if (pkt->service_size > DOTS_MAX)
-        hw_panic();
-
-    BrClrPtn();
-    
-    for (int row = 0; row < state->rows; row++) 
-        for (int col = 0; col < state->cols; col++) {
-            uint8_t cell = cell_map[col / 2];
-            bool upper = (col%2);
-            if (get_bit(state, pkt->data, row, col))
-                BrSetPtn(cell, (upper) ? 4 + row : row, true);
-        }
-
-    state->flags |= STATE_DIRTY;
-}
-
-void braille_handle_packet(srv_t *state, jd_packet_t *pkt) {
-
-    switch (pkt->service_command) {
-        default:
-            switch (service_handle_register(state, pkt, braille_regs)) {
-                case JD_DOT_MATRIX_REG_DOTS:
-                    if (JD_SET(JD_DOT_MATRIX_REG_DOTS) == pkt->service_command)
-                        handle_disp_write(state, pkt);
-                    break;
-            }
-    }
-}
-
-SRV_DEF(braille, JD_SERVICE_CLASS_DOT_MATRIX);
-void braille_init(const hbridge_api_t *params, uint16_t rows, uint16_t cols, uint8_t variant) {
-    SRV_ALLOC(braille);
-
-    state->api = params;
-    state->rows = rows;
-    state->cols = cols;
-    state->variant = variant;
-    state->flags = 0;
-    state->now = 0;
-
-    state->api->init();
-
-    uint16_t dot_bytes = 8 * cols; // this will need fixed up if we expand in future
-
-    if (dot_bytes > DOTS_MAX)
-        hw_panic();
-
-    memset(state->dots, 0, DOTS_MAX);
-
-    BrClrPtn();
-
-    BrClrAllDots(state);
-    delay(3000);
 }
