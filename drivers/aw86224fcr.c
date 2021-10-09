d176719ac198d2004ca8c4e6f61b13023fbaed98
James Devine <t-jamdev@microsoft.com>
2021-10-09 13:25:50 +0100

    fixup vibration service

--- drivers/aw86224fcr.c
+++ drivers/aw86224fcr.c
@@ -25,10 +25,32 @@
 #define AW86224_FCR_SYSCTRL2_REG            0x44
 #define AW86224_FCR_SYSCTRL2_VAL_12KHz      0x03
 
-static void aw86224fcr_write_amplitude(uint8_t amplitude, uint8_t duration_ms) {
-    // each command is 83.3 us of vibration
-    // 12 writes == 1 ms; add some buffer incase process is delayed.
-    for (int i = 0; i < (12 * duration_ms + 10); i++)
+static void aw86224fcr_go(void) {
+    // let's go!
+    i2c_write_reg(AW86224_FCR_ADDR, AW86224_FCR_PLAYCFG4_REG, AW86224_FCR_PLAYCFG4_VAL_GO);
+    target_wait_us(100);
+
+    int retry = 0;
+
+    // check the state machine of the part has moved into RTP mode.
+    while (1) {
+        uint8_t glb_state = i2c_read_reg(AW86224_FCR_ADDR, AW86224_FCR_GLBRD5_REG);
+
+        if (glb_state == 0b1000)
+            break;
+
+        target_wait_us(50);
+
+        retry++;
+
+        if (retry > 500)
+            target_reset();
+    }
+}
+
+static void aw86224fcr_write_amplitude(uint8_t amplitude, uint32_t duration_ms) {
+    aw86224fcr_go();
+    for (uint32_t i = 0; i < duration_ms * 12; i++)
         i2c_write_reg(AW86224_FCR_ADDR, AW86224_FCR_RTPDATA_REG, amplitude);
 }
 
@@ -54,24 +76,7 @@ static void aw86224fcr_init(void) {
     i2c_write_reg(AW86224_FCR_ADDR, AW86224_FCR_PLAYCFG3_REG, AW86224_FCR_PLAYCFG3_VAL_RTP);
     target_wait_us(100);
 
-    // let's go!
-    i2c_write_reg(AW86224_FCR_ADDR, AW86224_FCR_PLAYCFG4_REG, AW86224_FCR_PLAYCFG4_VAL_GO);
-    target_wait_us(100);
-
-    int retry = 0;
-
-    // check the state machine of the part has moved into RTP mode.
-    while (1) { 
-        uint8_t glb_state = i2c_read_reg(AW86224_FCR_ADDR, AW86224_FCR_GLBRD5_REG);
-
-        if (glb_state == 0b1000)
-            break;
-
-        retry++;
-
-        if (retry > 500)
-            hw_panic();
-    }
+    aw86224fcr_go();
 }
 
 const vibration_motor_api_t aw86224fcr = {
--- services/vibration.c
+++ services/vibration.c
@@ -21,8 +21,29 @@ REG_DEFINITION(                                   //
     REG_U8(JD_VIBRATION_MOTOR_REG_ENABLED),                 //
 )
 
+#if 1 
 void vibration_process(srv_t * state) {
-    if (!jd_should_sample(&state->now, 1000))
+    jd_vibration_motor_vibrate_t* curr = &state->sequence[state->idx];
+
+    if (curr->duration == 0) {
+        state->idx = (state->idx + 1) % MAX_SEQUENCE;
+        return;
+    }
+
+    // each speed tick is 8 ms in duration
+    // write for the next 8 ms
+    state->api->write_amplitude(curr->speed, 8);
+
+    if (!jd_should_sample(&state->now, 8000))
+        return;
+    
+    curr->duration--;
+}
+#endif
+
+#if 0
+void vibration_process(srv_t * state) {
+    if (!jd_should_sample(&state->now, 8000))
         return;
 
     jd_vibration_motor_vibrate_t* curr = &state->sequence[state->idx];
@@ -32,11 +53,14 @@ void vibration_process(srv_t * state) {
         return;
     }
     
-    // each speed tick is 8 ms in duration 
+    DMESG("AMP write %d %d", curr->duration, curr->speed);
+
+    // each speed tick is 8 ms in duration
+    // write for the next 8 ms
     state->api->write_amplitude(curr->speed, 8);
     curr->duration--;
 }
-
+#endif
 
 static void handle_vibrate_cmd(srv_t *state, jd_packet_t *pkt) {
     memset(state->sequence, 0, sizeof(state->sequence));
@@ -45,7 +69,6 @@ static void handle_vibrate_cmd(srv_t *state, jd_packet_t *pkt) {
 }
 
 void vibration_handle_packet(srv_t *state, jd_packet_t *pkt) {
-
     switch(pkt->service_command) {
         case JD_VIBRATION_MOTOR_CMD_VIBRATE:
             handle_vibrate_cmd(state, pkt);
@@ -62,6 +85,8 @@ void vibration_service_init(const vibration_motor_api_t *api) {
     SRV_ALLOC(vibration);
     state->api = api;
     state->enabled = 1;
+
+    memset(state->sequence, 0x00, sizeof(state->sequence));
 
     state->api->init();
 }
