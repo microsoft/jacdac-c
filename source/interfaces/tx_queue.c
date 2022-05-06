b187a9aec3964ae261d304effd5e2eb11dfe0a12
Michal Moskal <michal@moskal.me>
2022-05-05 21:49:27 -0700

    use jd_send_frame() if possible

--- source/interfaces/tx_queue.c
+++ source/interfaces/tx_queue.c
@@ -3,8 +3,17 @@
 
 #include "jd_protocol.h"
 
+#ifndef JD_USB_SEND
+#define JD_USB_SEND(...) ((void)0)
+#endif
+
+#if JD_SEND_FRAME
+static jd_frame_t tx_acc_buffer;
+#else
 static jd_frame_t *sendFrame;
 static uint8_t bufferPtr, isSending;
+#define tx_acc_buffer sendFrame[bufferPtr]
+#endif
 
 #if JD_RAW_FRAME
 uint8_t rawFrameSending;
@@ -21,55 +30,66 @@ int jd_send_frame_raw(jd_frame_t *f) {
         ERROR("loopback rx ovf");
     int r = jd_queue_push(send_queue, f);
     if (r)
-        ERROR("send_frm ovf");
+        ERROR("send ovf");
     jd_packet_ready();
     return r;
 }
 int jd_send_frame(jd_frame_t *f) {
-#ifdef JD_USB_SEND
     JD_USB_SEND(f);
-#endif
     return jd_send_frame_raw(f);
 }
 #endif
 
-static int has_oob_frame(void) {
+int jd_tx_is_idle() {
 #if JD_RAW_FRAME
     if (rawFrame || rawFrameSending)
-        return 1;
+        return 0;
 #endif
 #if JD_SEND_FRAME
     if (q_sending || jd_queue_front(send_queue))
-        return 1;
+        return 0;
+#else
+    if (isSending)
+        return 0;
 #endif
 
-    return 0;
-}
+    if (tx_acc_buffer.size != 0)
+        return 0;
 
-int jd_tx_is_idle() {
-    return !has_oob_frame() && !isSending && sendFrame[bufferPtr].size == 0;
+    return 1;
 }
 
 void jd_tx_init(void) {
-    if (!sendFrame) {
-        sendFrame = (jd_frame_t *)jd_alloc(sizeof(jd_frame_t) * 2);
 #if JD_SEND_FRAME
+    if (!send_queue)
         send_queue = jd_queue_alloc(sizeof(jd_frame_t) * 2);
+#else
+    if (!sendFrame)
+        sendFrame = (jd_frame_t *)jd_alloc(sizeof(jd_frame_t) * 2);
 #endif
-    }
 }
 
 int jd_send(unsigned service_num, unsigned service_cmd, const void *data, unsigned service_size) {
     if (target_in_irq())
         jd_panic();
 
-    void *trg = jd_push_in_frame(&sendFrame[bufferPtr], service_num, service_cmd, service_size);
+    void *trg = jd_push_in_frame(&tx_acc_buffer, service_num, service_cmd, service_size);
     if (!trg) {
+#if JD_SEND_FRAME
+        jd_send_frame(&tx_acc_buffer);
+        jd_reset_frame(&tx_acc_buffer);
+        trg = jd_push_in_frame(&tx_acc_buffer, service_num, service_cmd, service_size);
+        JD_ASSERT(trg != NULL);
+#else
         ERROR("send ovf");
         return -1;
+#endif
     }
-    if (data)
+
+    if (service_size > 0) {
+        JD_ASSERT(data != NULL);
         memcpy(trg, data, service_size);
+    }
 
     return 0;
 }
@@ -91,12 +111,27 @@ jd_frame_t *jd_tx_get_frame(void) {
         q_sending = true;
         return f;
     }
-#endif
+#else
     if (isSending == 1) {
         isSending = 2;
         return &sendFrame[bufferPtr ^ 1];
     }
+#endif
     return NULL;
+}
+
+static void poke_ready(void) {
+#if JD_RAW_FRAME
+    if (rawFrame)
+        jd_packet_ready();
+#endif
+#if JD_SEND_FRAME
+    if (jd_queue_front(send_queue))
+        jd_packet_ready();
+#else
+    if (isSending)
+        jd_packet_ready();
+#endif
 }
 
 // bridge between phys and queue imp, marks as sent.
@@ -104,51 +139,49 @@ void jd_tx_frame_sent(jd_frame_t *pkt) {
 #if JD_RAW_FRAME
     if (rawFrameSending) {
         rawFrameSending = false;
-        goto done;
+        poke_ready();
+        return;
     }
 #endif
 
 #if JD_SEND_FRAME
-    if (q_sending) {
-        jd_queue_shift(send_queue);
-        q_sending = false;
-        goto done;
-    }
-#endif
-
+    JD_ASSERT(q_sending);
+    jd_queue_shift(send_queue);
+    q_sending = false;
+#else
     JD_ASSERT(isSending == 2);
     isSending = 0;
-    goto done; // avoid warning
+#endif
 
-done:
-    if (isSending == 1 || has_oob_frame())
-        jd_packet_ready();
+    poke_ready();
 }
 
 void jd_tx_flush() {
     if (target_in_irq())
         jd_panic();
-    if (isSending) {
-        // jd_packet_ready();
-        return;
-    }
-    jd_frame_t *f = &sendFrame[bufferPtr];
+    jd_frame_t *f = &tx_acc_buffer;
     if (f->size == 0)
         return;
+#if !JD_SEND_FRAME
+    if (isSending)
+        return;
+#endif
     f->device_identifier = jd_device_id();
     jd_compute_crc(f);
 
+#if JD_SEND_FRAME
+    jd_send_frame(f);
+#else
     if (jd_rx_frame_received_loopback(f))
         ERROR("loopback rx ovf");
 
-#ifdef JD_USB_SEND
     JD_USB_SEND(f);
-#endif
 
     bufferPtr ^= 1;
     isSending = 1;
     jd_packet_ready();
+#endif
     jd_services_packet_queued();
 
-    jd_reset_frame(&sendFrame[bufferPtr]);
+    jd_reset_frame(&tx_acc_buffer);
 }
