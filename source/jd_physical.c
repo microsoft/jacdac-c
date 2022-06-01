2d768a8d8d45967c6b3bb61f8977a01902225d25
Michal Moskal <michal@moskal.me>
2022-06-01 14:07:57 -0700

    assert not valid on stm

--- source/jd_physical.c
+++ source/jd_physical.c
@@ -140,7 +140,7 @@ void jd_line_falling() {
 
     // target_disable_irq();
     // no need to disable IRQ - we're at the highest IRQ level
-    if (status & (JD_STATUS_RX_ACTIVE | JD_STATUS_TX_ACTIVE))
+    if (status & JD_STATUS_RX_ACTIVE)
         jd_panic();
     status |= JD_STATUS_RX_ACTIVE;
 
