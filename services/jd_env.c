c70b2a8e42ae1e95611f1b1589d68399b58bba4f
Michal Moskal <michal@moskal.me>
2022-06-02 14:12:15 -0700

    send error with readings

--- services/jd_env.c
+++ services/jd_env.c
@@ -13,8 +13,11 @@ void env_sensor_process(srv_t *state) {
     sensor_process(state);
     if (sensor_should_stream(state)) {
         const env_reading_t *env = sensor_get_reading(state);
-        if (env)
+        if (env) {
             jd_send(state->service_index, JD_GET(JD_REG_READING), &env->value, sizeof(env->value));
+            jd_send(state->service_index, JD_GET(JD_REG_READING_ERROR), &env->error,
+                    sizeof(env->error));
+        }
     }
 }
 
