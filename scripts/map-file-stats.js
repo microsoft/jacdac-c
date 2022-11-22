6d5b7b4376e864323efbf98e916e51890d544b42
Michal Moskal <michal@moskal.me>
2022-11-21 21:03:20 -0800

    add stats script

---
+++ scripts/map-file-stats.js
@@ -0,0 +1,83 @@
+let fs = require("fs")
+let sums = { }
+let inprog = false
+let inram = false
+let indata = false
+let fun = ""
+let dofun = process.argv[3] == "-fun"
+for (let line of fs.readFileSync(process.argv[2], "utf8").split(/\r?\n/)) {
+  if (/\*fill\*/.test(line)) continue
+  if (/^r[oa]m\s/.test(line)) continue
+  let m = /^ \.(\w+)/.exec(line)
+  if (m) {
+    if (m[1] == "text" || m[1] == "binmeta" || m[1] == "rodata" || m[1] == "data" || m[1] == "isr_vector")
+      inprog = true
+    else
+      inprog = false
+    if (m[1] == "data" || m[1] == "bss")
+      inram = true
+    else
+      inram = false
+    if (m[1] == "data" || m[1] == "rodata")
+      indata = true
+    else
+      indata = false
+    fun = line.slice(m[1].length + 2).replace(/\s.*/, "")
+  }
+  if (!inprog && !inram) continue
+  m = /\s+(0x00[a-f0-9]+)\s+(0x[a-f0-9]+)\s+(.*)/.exec(line)
+  if (!m) continue
+  let addr = parseInt(m[1])
+  let size = parseInt(m[2])
+  if (!addr || !size) continue
+  let name = m[3]
+  if (/load address/.test(name)) continue
+  if (name == "xr" || name == "xrw") continue
+  name = name.replace(/.*\/lib/, "lib")
+    .replace(/\(.*/, "") // can remove
+
+  if (dofun) name += fun
+
+  function doSum(pref) {
+      const n = pref + "." + name
+      if (!sums[n]) sums[n] = 0
+      sums[n] += size
+      if (!sums[pref + ".TOTAL"]) sums[pref + ".TOTAL"] = 0
+      sums[pref + ".TOTAL"] += size
+  }
+
+  if (indata)
+    doSum("DATA")
+  else if (inprog)
+    doSum("TEXT")
+
+  if (inprog)
+    doSum("FLASH")
+
+  if (inram)
+    doSum("RAM")
+
+  let pref = inram ? "RAM." : ""
+
+  //if (!inram && size > 100) console.log(line)
+
+  name = pref + name
+
+  //console.log(name, size, line)
+
+}
+
+function order(pref) {
+  const k = Object.keys(sums).filter(s => s.startsWith(pref + "."))
+  k.sort((a, b) => sums[a] - sums[b])
+  for (let kk of k) {
+    console.log(kk, sums[kk])
+  }
+  console.log("\n")
+}
+
+order("RAM")
+order("DATA")
+order("TEXT")
+order("FLASH")
+
