a6e542e02f87a19c8c977b60743ef1fb6653b716
ssande-fwd <ssande@forwardedu.com>
2025-03-21 11:22:17 -0400

    do not process the version number if it's generated based on commit rather than tag i.e. v959x563-20250321-1122

--- scripts/git-version.sh
+++ scripts/git-version.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 D=`date +%Y%m%d-%H%M`
 TAG=`git describe --dirty --tags --match 'v[0-9]*' --always | sed -e 's/^v//; s/-dirty/-'"$D/"`
-SUFF=`echo $TAG | sed -e 's/[^-]*//'`
-if [ "X$SUFF" = "X" ] ; then
-  # if no -hash or -dirty, just take the tag
-  echo "$TAG"
+if [[ "$TAG" =~ ^[0-9a-f]+-[0-9]{8}-[0-9]{4}$ ]]; then
+  echo "$TAG" # Return the hash as is
 else
-  # otherwise, bump the last number of semver
-  # in semver 0.0.7 > 0.0.7-something (think 0.0.7-rc4)
-  PREF=`echo $TAG | sed -e 's/-.*//; s/\(.*\)\.[0-9]*/\1/'`
-  LAST=`echo $TAG | sed -e 's/-.*//; s/.*\.//'`
-  echo "$PREF.$(($LAST+1))$SUFF"
+  SUFF=`echo $TAG | sed -e 's/[^-]*//'`
+  if [ "X$SUFF" = "X" ] ; then
+    # if no -hash or -dirty, just take the tag
+    echo "$TAG"
+  else
+    # otherwise, bump the last number of semver
+    # in semver 0.0.7 > 0.0.7-something (think 0.0.7-rc4)
+    PREF=`echo $TAG | sed -e 's/-.*//; s/\(.*\)\.[0-9]*/\1/'`
+    LAST=`echo $TAG | sed -e 's/-.*//; s/.*\.//'`
+    echo "$PREF.$(($LAST+1))$SUFF"
+  fi
 fi
